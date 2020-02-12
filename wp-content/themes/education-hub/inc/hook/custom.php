c1903351643bd5bb0e1e5aeadc6af394ae9a2a1d
annq7128 <annq7128@co-well.com.vn>
2020-02-12 14:21:11 +0700

    Remove Site Info

--- wp-content/themes/education-hub/inc/hook/custom.php
+++ wp-content/themes/education-hub/inc/hook/custom.php
@@ -214,11 +214,6 @@ if ( ! function_exists( 'education_hub_footer_copyright' ) ) :
 	        <?php echo $copyright_text; ?>
 	      </div><!-- .copyright -->
 	    <?php endif; ?>
-	    <div class="site-info">
-	      <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'education-hub' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'education-hub' ), 'WordPress' ); ?></a>
-	      <span class="sep"> | </span>
-			<?php printf( esc_html__( '%1$s by %2$s', 'education-hub' ), 'Education Hub', '<a href="' . esc_url( 'https://wenthemes.com/' ) . '" rel="designer" target="_blank">WEN Themes</a>' ); ?>
-	    </div><!-- .site-info -->
 	    <?php
 
 	}
